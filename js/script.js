5b57c60c687464135e9509f6635eb63ee1679226
Willian Romero <ra23059@ues.edu.sv>
2024-11-20 00:44:53 -0600

    se arreglaron los errores de clases se moodico el style.css solucionando errores se agrego codigo en js/script.js y tambien se empezo a hacer uso de responsive modified:   Trabajo_Manejo.html modified:   css/style.css modified:   js/script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('.menu');
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#menu-btn').onclick = () => {
+    menu.classList.toggle('active');
+    searchForm.classList.remove('active');
+};
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    menu.classList.remove('active');
+};
+
+
+window.addEventListener('click', (e) => {
+    if (!e.target.closest('.menu') && !e.target.closest('#menu-btn')) {
+        menu.classList.remove('active');
+    }
+    if (!e.target.closest('.search-form') && !e.target.closest('#search-btn')) {
+        searchForm.classList.remove('active');
+    }
+});
