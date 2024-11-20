a1f7711c2fa160a47e788256926bdb6b09836586
Manfredy <gb23002@ues.edu.sv>
2024-11-20 16:48:59 -0600

    test de texto de menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 let menu = document.querySelector('.menu');
 let searchForm = document.querySelector('.search-form');
+let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#menu-btn').onclick = () => {
     menu.classList.toggle('active');
     searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
 };
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     menu.classList.remove('active');
+    cartItem.classList.remove('active');
 };
 
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    menu.classList.remove('active');
+    searchForm.classList.remove('active');
+};
 
 window.addEventListener('click', (e) => {
     if (!e.target.closest('.menu') && !e.target.closest('#menu-btn')) {
@@ -20,3 +28,8 @@ window.addEventListener('click', (e) => {
         searchForm.classList.remove('active');
     }
 });
+window.onscroll = () =>{
+    menu.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
