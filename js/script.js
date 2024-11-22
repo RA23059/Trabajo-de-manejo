25080b61a25883576c9926ef97263f22501b2fc6
Willian Romero <ra23059@ues.edu.sv>
2024-11-21 18:49:38 -0600

    Se Solucionaron los errores de Trabajo_Manejo.html modified:   Trabajo_Manejo.html se modifico el css y se Solucionaron errores modified:   css/style.css se agrego mas imagenes para el menu y los productos new file:   imagenes/Cafe-Don-Justo.jpg new file:   imagenes/Cafe-chunguita.jpg se soluciono los errores en el script.js de los botones de menu y Buscar modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 let menu = document.querySelector('.menu');
 let searchForm = document.querySelector('.search-form');
-let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#menu-btn').onclick = () => {
     menu.classList.toggle('active');
     searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
 };
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     menu.classList.remove('active');
-    cartItem.classList.remove('active');
-};
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    menu.classList.remove('active');
-    searchForm.classList.remove('active');
 };
 
 window.addEventListener('click', (e) => {
@@ -28,8 +19,8 @@ window.addEventListener('click', (e) => {
         searchForm.classList.remove('active');
     }
 });
-window.onscroll = () =>{
+
+window.onscroll = () => {
     menu.classList.remove('active');
     searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+};
